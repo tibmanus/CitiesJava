2f7590c35220c3077140e0746f13692629179c53
Tibor Beke <tibmanus@gmail.com>
2018-05-05 23:32:18 +0200

    Make City and Coordinates parcelable
    - So they can be passed among activities

--- app/src/main/java/tech/tibor/cities_java/City.java
+++ app/src/main/java/tech/tibor/cities_java/City.java
@@ -1,6 +1,9 @@
 package tech.tibor.cities_java;
 
-public class City {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class City implements Parcelable{
 
     String _id;
 
@@ -10,10 +13,35 @@ public class City {
 
     Coordinates coord;
 
-    public class Coordinates {
-
-        Double lon;
-
-        Double lat;
+    protected City(Parcel in) {
+        _id = in.readString();
+        country = in.readString();
+        name = in.readString();
+        coord = in.readParcelable(Coordinates.class.getClassLoader());
     }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(_id);
+        dest.writeString(country);
+        dest.writeString(name);
+        dest.writeParcelable(coord, flags);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<City> CREATOR = new Creator<City>() {
+        @Override
+        public City createFromParcel(Parcel in) {
+            return new City(in);
+        }
+
+        @Override
+        public City[] newArray(int size) {
+            return new City[size];
+        }
+    };
 }
---
+++ app/src/main/java/tech/tibor/cities_java/Coordinates.java
@@ -0,0 +1,57 @@
+package tech.tibor.cities_java;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Coordinates implements Parcelable {
+
+    Double lon;
+
+    Double lat;
+
+    protected Coordinates(Parcel in) {
+        if (in.readByte() == 0) {
+            lon = null;
+        } else {
+            lon = in.readDouble();
+        }
+        if (in.readByte() == 0) {
+            lat = null;
+        } else {
+            lat = in.readDouble();
+        }
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        if (lon == null) {
+            dest.writeByte((byte) 0);
+        } else {
+            dest.writeByte((byte) 1);
+            dest.writeDouble(lon);
+        }
+        if (lat == null) {
+            dest.writeByte((byte) 0);
+        } else {
+            dest.writeByte((byte) 1);
+            dest.writeDouble(lat);
+        }
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Coordinates> CREATOR = new Creator<Coordinates>() {
+        @Override
+        public Coordinates createFromParcel(Parcel in) {
+            return new Coordinates(in);
+        }
+
+        @Override
+        public Coordinates[] newArray(int size) {
+            return new Coordinates[size];
+        }
+    };
+}
