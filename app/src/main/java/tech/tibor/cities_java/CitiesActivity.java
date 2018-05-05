aef1f446e4c8fa0b189ed9fb4e0ceda9037b7dfb
Tibor Beke <tibmanus@gmail.com>
2018-05-05 23:37:01 +0200

    Start MapsActivity when a city is selected and show marker and label at coordinates

--- app/src/main/java/tech/tibor/cities_java/CitiesActivity.java
+++ app/src/main/java/tech/tibor/cities_java/CitiesActivity.java
@@ -1,11 +1,14 @@
 package tech.tibor.cities_java;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class CitiesActivity
         extends AppCompatActivity
     implements CitiesFragment.OnListFragmentInteractionListener {
+
+    public static final String EXTRA_MESSAGE = "tibor.tech.City";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +18,8 @@ public class CitiesActivity
 
     @Override
     public void onListFragmentInteraction(City item) {
-
+        Intent intent = new Intent(this, MapsActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, item);
+        startActivity(intent);
     }
 }
--- app/src/main/java/tech/tibor/cities_java/MapsActivity.java
+++ app/src/main/java/tech/tibor/cities_java/MapsActivity.java
@@ -2,6 +2,7 @@ package tech.tibor.cities_java;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -38,9 +39,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        City city = getIntent().getParcelableExtra(CitiesActivity.EXTRA_MESSAGE);
+        if (city != null) {
+            LatLng coordinates = new LatLng(city.coord.lat, city.coord.lon);
+            mMap.addMarker(new MarkerOptions().position(coordinates).title("Marker in " + city.name));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(coordinates));
+        }
     }
 }
