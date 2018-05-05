124a3e6e0cb765793d0eb49ba4b06d5ba34eaa31
Tibor Beke <tibmanus@gmail.com>
2018-05-05 22:04:56 +0200

    Init

---
+++ app/src/main/java/tech/tibor/cities_java/CitiesActivity.java
@@ -0,0 +1,13 @@
+package tech.tibor.cities_java;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CitiesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cities);
+    }
+}
